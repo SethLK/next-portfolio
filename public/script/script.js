1af80d571853adf34babcd0b1f6cde68b1dfd354
SethLK <sethlk2006@gmail.com>
2023-10-02 14:44:19 +0630

    Edit script js

--- public/script/script.js
+++ public/script/script.js
@@ -1,25 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuLi = document.querySelector(".menu-li");
-    const dropdownMenu = document.querySelector(".dropdown-menu");
-  
-    // Function to hide the dropdown menu
-    function hideDropdownMenu() {
+  const menuLi = document.querySelector(".menu-li");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+
+  // Function to hide the dropdown menu
+  dropdownMenu.style.display = "none";
+
+  // Function to toggle the dropdown menu
+  function toggleDropdownMenu() {
+    if (dropdownMenu.style.display === "none") {
+      dropdownMenu.style.display = "flex";
+    } else {
       dropdownMenu.style.display = "none";
     }
-  
-    // Function to toggle the dropdown menu
-    function toggleDropdownMenu() {
-      if (dropdownMenu.style.display === "none") {
-        dropdownMenu.style.display = "flex";
-      } else {
-        dropdownMenu.style.display = "none";
-      }
-    }
-  
-    menuLi.addEventListener("click", function() {
-      toggleDropdownMenu();
-    });
-  
-    
+  }
+
+  menuLi.addEventListener("click", function() {
+    toggleDropdownMenu();
   });
-  
+
+  // Close the dropdown menu when a navigation link is clicked
+  const navigationLinks = document.querySelectorAll(".links");
+  navigationLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+      dropdownMenu.style.display = "none";
+    });
+  });
+});
