4bf7b03a4a5765cddb96b56262e2c09d30b697ff
SethLK <sethlk2006@gmail.com>
2023-10-15 21:51:15 +0630

    final commit

--- public/script/script.js
+++ public/script/script.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const dropdownMenu = document.querySelector(".dropdown-menu");
 
   // Function to hide the dropdown menu
-  dropdownMenu.style.display = "none";
+  function hideDropdownMenu() {
+    dropdownMenu.style.display = "none";
+  }
 
   // Function to toggle the dropdown menu
   function toggleDropdownMenu() {
@@ -18,11 +20,5 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleDropdownMenu();
   });
 
-  // Close the dropdown menu when a navigation link is clicked
-  const navigationLinks = document.querySelectorAll(".links");
-  navigationLinks.forEach(function(link) {
-    link.addEventListener("click", function() {
-      dropdownMenu.style.display = "none";
-    });
-  });
+  
 });
